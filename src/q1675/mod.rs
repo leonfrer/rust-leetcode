b2409b9b0df84fb0b55c541e20bacd3d2e73abe7
Leon <51883574+leonfrer@users.noreply.github.com>
2023-02-27 13:35:15 +0800

    1675

--- src/q1675/mod.rs
+++ src/q1675/mod.rs
@@ -1,1 +1,37 @@
+struct Solution {}
 
+use std::collections::BinaryHeap;
+impl Solution {
+    pub fn minimum_deviation(nums: Vec<i32>) -> i32 {
+        let mut heap = BinaryHeap::with_capacity(nums.len());
+        let mut min = i32::MAX;
+        let mut answer = i32::MAX;
+        for num in nums.into_iter() {
+            let n = if num & 1 == 1 { num * 2 } else { num };
+            min = min.min(n);
+            heap.push(n);
+        }
+        while let Some(num) = heap.pop() {
+            answer = answer.min(num - min);
+            if num & 1 == 1 {
+                break;
+            }
+            let n = num / 2;
+            min = min.min(n);
+            heap.push(n);
+        }
+        answer
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(Solution::minimum_deviation(vec![1, 2, 3, 4]), 1);
+        assert_eq!(Solution::minimum_deviation(vec![4, 1, 5, 20, 3]), 3);
+        assert_eq!(Solution::minimum_deviation(vec![2, 10, 8]), 3);
+    }
+}
