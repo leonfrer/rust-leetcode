ff2e17deabdd82c73078f1bf89267473953462fa
Leon <leonfrer@gmail.com>
2023-03-04 22:23:40 +0800

    q2444 start

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@ mod q443;
 mod q502;
 mod q652;
 mod q912;
+mod q2444;
---
+++ src/q2444/mod.rs
@@ -0,0 +1,12 @@
+struct Solution {}
+
+impl Solution {
+    pub fn count_subarrays(nums: Vec<i32>, min_k: i32, max_k: i32) -> i64 {
+        0
+    }
+}
+
+#[test]
+fn test() {
+    // 
+}
